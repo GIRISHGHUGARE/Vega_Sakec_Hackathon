c3a7a6538548a2c8ae3e61b3ec2405076b6eac0a
Girish Ghugare <116804758+GIRISHGHUGARE@users.noreply.github.com>
2024-03-06 23:35:01 +0530

    Add files via upload

---
+++ Company_side.aspx.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace TechDynamos
+{
+    public partial class WebForm3 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            CompanyName.ReadOnly = true;
+            CompanyEmail.ReadOnly = true;
+            CompanyAddr.ReadOnly = true;
+            CompanyContact.ReadOnly = true;
+            AboutUs.ReadOnly = true;
+        }
+
+        protected void ImageButton1_Click(object sender, ImageClickEventArgs e)
+        {
+            CompanyName.ReadOnly = false;
+            CompanyEmail.ReadOnly = false;
+            CompanyAddr.ReadOnly = false;
+            CompanyContact.ReadOnly = false;
+            AboutUs.ReadOnly = false;
+            CompanyName.Style["border-color"] = "black";
+            CompanyEmail.Style["border-color"] = "black";
+            CompanyAddr.Style["border-color"] = "black";
+            CompanyContact.Style["border-color"] = "black";
+            AboutUs.Style["border-color"] = "black";
+            CompanyName.Style["border-color"] = "black";
+            CompanyEmail.Style["border-color"] = "black";
+            CompanyAddr.Style["border-color"] = "black";
+            CompanyContact.Style["border-color"] = "black";
+            AboutUs.Style["border-color"] = "black";
+            CompanyName.Style["color"] = "black";
+            CompanyEmail.Style["color"] = "black";
+            CompanyAddr.Style["color"] = "black";
+            CompanyContact.Style["color"] = "black";
+            AboutUs.Style["color"] = "black";
+            CompanyName.Style["color"] = "black";
+            CompanyEmail.Style["color"] = "black";
+            CompanyAddr.Style["color"] = "black";
+            CompanyContact.Style["color"] = "black";
+            AboutUs.Style["color"] = "black";
+        }
+    }
+}
