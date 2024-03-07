2115074074a3bd75a0cbbe1e2c7309e3a03e0575
Girish Ghugare <116804758+GIRISHGHUGARE@users.noreply.github.com>
2024-03-07 12:11:52 +0530

    Add files via upload

--- WebForm4.aspx.cs
+++ WebForm4.aspx.cs
@@ -2,15 +2,18 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
+using System.Drawing;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Xml.Linq;
 
 namespace TechDynamo
 {
     public partial class WebForm4 : System.Web.UI.Page
     {
+        string name, uname;
         SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=D:\TechDynamo\TechDynamo\TechDynamo\App_Data\Database1.mdf;Integrated Security=True");
 
         protected void Page_Load(object sender, EventArgs e)
@@ -32,8 +35,24 @@ namespace TechDynamo
         protected void Button1_Click(object sender, EventArgs e)
         {
             con.Open();
-            string que = "insert into msg values ('User','" + TextBox6.Text + "')";
-            SqlCommand cmd1 = new SqlCommand(que, con);
+            if (Session["user"] != null)
+            {
+                uname = Session["user"].ToString();
+            }
+            string que = "select * from student where email = '" + uname + "'";
+            SqlCommand cmd2 = new SqlCommand(que, con);
+            SqlDataReader sr = cmd2.ExecuteReader();
+            while (sr.Read())
+            {
+               name = sr.GetValue(1).ToString();
+                
+            }
+            con.Close();
+
+            con.Open();
+
+            string que1 = "insert into msg values ('"+name+"','" + TextBox6.Text + "')";
+            SqlCommand cmd1 = new SqlCommand(que1, con);
             cmd1.ExecuteNonQuery();
             con.Close();
             Response.Redirect("WebForm4.aspx");
---
+++ stud_applied.aspx.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml.Linq;
+
+namespace TechDynamo
+{
+    public partial class stud_applied : System.Web.UI.Page
+    {
+        string uname,comp_name;
+        SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=D:\TechDynamo\TechDynamo\TechDynamo\App_Data\Database1.mdf;Integrated Security=True");
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            con.Open();
+            if (Session["user"] != null)
+            {
+                uname = Session["user"].ToString();
+            }
+
+            string que = "select * from company where email = '" + uname + "'";
+            SqlCommand cmd2 = new SqlCommand(que, con);
+            SqlDataReader sr = cmd2.ExecuteReader();
+            while (sr.Read())
+            {
+                comp_name = sr.GetValue(1).ToString();
+               
+            }
+            con.Close();
+        
+
+            con.Open();
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "select * from application where comp_name = '"+comp_name+"'";
+            cmd.ExecuteNonQuery();
+            DataTable dt = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            da.Fill(dt);
+            DataList1.DataSource = dt;
+            DataList1.DataBind();
+            con.Close();
+        }
+    }
+}
